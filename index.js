ade00ec59dbc494ea9f6b48cba403299d8415971
Noteworthy Software, Inc <online@noteworthysoftware.com>
2020-10-18 04:38:09 -0400

    Create a default entry point for the Leaflet src
    When directly adding the raw Leaflet repos to a project, the `dist` folder lacks any bundled
    source code (usually built and bundled via rollup scripts). The `package.json` designated
    entry point will fail in this circumstance. However, Node can successfully fall back to an
    alternate entry point, which is created by this update.

---
+++ index.js
@@ -0,0 +1,7 @@
+// This file provides a default entry point when the raw leaflet repos
+// is added directly to a project. In this configuration, you might
+// want to enable webpack's ProvidePlugin, like so:
+//
+// new webpack.ProvidePlugin({L:'leaflet','window.L':'leaflet'})
+//
+export * from './src/Leaflet.js';
